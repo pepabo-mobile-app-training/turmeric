199757f09f5da5017fcc29ff4743353c625dc385
ogidow <syuta.ogido@pepabo.com>
2016-09-26 18:24:30 +0900

    postメソドに対応

--- Turmeric/Classes/Helpers/APIClient.swift
+++ Turmeric/Classes/Helpers/APIClient.swift
@@ -6,11 +6,12 @@ import SwiftyJSON
 class APIClient {
     static private let baseUrl = "http://currry.xyz"
     
-    static func request(endpoint: Endpoint, handler: @escaping (_ json: JSON) -> Void) {
+    static func request(endpoint: Endpoint, parameters: Dictionary<String, String>?, handler: @escaping (_ json: JSON) -> Void) {
         let method = endpoint.method()
         let url = fullURL(endpoint: endpoint)
         
-        Alamofire.request(url, method: method).validate(statusCode: 200...299).responseJSON { response in
+        
+        Alamofire.request(url, method: method, parameters: parameters).validate(statusCode: 200...299).responseJSON { response in
             switch response.result {
             case .success(let value):
                 handler(JSON(value))
@@ -18,6 +19,10 @@ class APIClient {
                 print(error)
             }
         }
+    }
+    
+    static func request(endpoint: Endpoint, handler: @escaping (_ json: JSON) -> Void){
+        request(endpoint: endpoint, parameters: nil, handler: handler)
     }
     
     static private func fullURL(endpoint: Endpoint) -> String {
@@ -28,11 +33,13 @@ class APIClient {
 enum Endpoint {
     case Auth
     case UsersFeed
+    case UsersCreate
     
     func method() -> HTTPMethod {
         switch self {
         case .Auth: return .post
         case .UsersFeed: return .get
+        case .UsersCreate: return .post
         }
     }
     
@@ -40,6 +47,7 @@ enum Endpoint {
         switch self {
         case .Auth: return "/api/auth"
         case .UsersFeed: return "/api/feed"
+        case .UsersCreate: return "/api/users"
         }
     }
 }
