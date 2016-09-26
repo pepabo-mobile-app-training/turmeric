6ecef621689bd8feeff70a7ca44aa544497e26d5
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-09-26 14:57:02 +0900

    Initial commit

---
+++ Turmeric/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Turmeric
+//
+//  Created by usr0600433 on 2016/09/26.
+//  Copyright © 2016年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
