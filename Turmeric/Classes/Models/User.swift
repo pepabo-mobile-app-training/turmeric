d194497fd56a5ed4dec45e42b4a032e48b0bfad0
ogidow <syuta.ogido@pepabo.com>
2016-09-27 11:25:29 +0900

    Userモデル

---
+++ Turmeric/Classes/Models/User.swift
@@ -0,0 +1,28 @@
+//
+//  User.swift
+//  Turmeric
+//
+//  Created by usr0600438 on 2016/09/27.
+//  Copyright © 2016年 GMO Pepabo. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import Alamofire
+
+class User {
+    
+    var id: Int
+    var name: String
+    
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+    
+    static func createUser(parameters: Parameters, handler: @escaping ((User) -> Void)){
+        APIClient.request(endpoint: Endpoint.UsersCreate, parameters: parameters) { json in
+            handler(User(id: json["user"]["id"].intValue, name: json["user"]["name"].stringValue))
+        }
+    }
+}
