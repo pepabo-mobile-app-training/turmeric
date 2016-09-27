9b64de2b3a39d78ed4b53c29677adcfb693e7f45
ogidow <syuta.ogido@gmail.com>
2016-09-27 12:28:30 +0900

    リクエストヘッダーを指定できるように

--- Turmeric/Classes/Helpers/APIClient.swift
+++ Turmeric/Classes/Helpers/APIClient.swift
@@ -10,8 +10,23 @@ class APIClient {
         let method = endpoint.method()
         let url = fullURL(endpoint: endpoint)
         
-        
+
         Alamofire.request(url, method: method, parameters: parameters).validate(statusCode: 200...299).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                handler(JSON(value))
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    static func request(endpoint: Endpoint, parameters: Parameters?, headers: HTTPHeaders, handler: @escaping (_ json: JSON) -> Void) {
+        let method = endpoint.method()
+        let url = fullURL(endpoint: endpoint)
+        
+        
+        Alamofire.request(url, method: method, parameters: parameters, headers: headers).validate(statusCode: 200...299).responseJSON { response in
             switch response.result {
             case .success(let value):
                 handler(JSON(value))
@@ -24,6 +39,11 @@ class APIClient {
     static func request(endpoint: Endpoint, handler: @escaping (_ json: JSON) -> Void){
         request(endpoint: endpoint, parameters: nil, handler: handler)
     }
+    
+    static func request(endpoint: Endpoint, headers: HTTPHeaders, handler: @escaping (_ json: JSON) -> Void){
+        request(endpoint: endpoint, parameters: nil, headers: headers, handler: handler)
+    }
+    
     static private func fullURL(endpoint: Endpoint) -> String {
         return baseUrl + endpoint.path()
     }
@@ -33,20 +53,56 @@ enum Endpoint {
     case Auth
     case UsersFeed
     case UsersCreate
+    case UsersMicropost
+    case UsersFollowing
+    case UsersFollowers
+    case UsersShow
+    case UsersUpdate
+    
+    //Microposts
+    case MicropostsPost
+    case MicropostsShow
+    case MicropostsDelete
+    case Feed
+    
     
     func method() -> HTTPMethod {
         switch self {
         case .Auth: return .post
         case .UsersFeed: return .get
         case .UsersCreate: return .post
+        case .UsersMicropost: return .get
+        case .UsersFollowing: return .get
+        case .UsersFollowers: return .get
+        case .UsersShow: return .get
+        case .UsersUpdate: return .patch
+            
+        case .MicropostsPost: return .post
+        case .MicropostsShow: return .get
+        case .MicropostsDelete: return .delete
+        case .Feed: return .get
         }
     }
     
-    func path() -> String {
+    func path(id: Int?) -> String {
         switch self {
         case .Auth: return "/api/auth"
         case .UsersFeed: return "/api/feed"
         case .UsersCreate: return "/api/users"
+        case .UsersMicropost: return "/api/users/\(id)/microposts"
+        case .UsersFollowing: return "/api/users/\(id)/following"
+        case .UsersFollowers: return "/api/users/\(id)/followers"
+        case .UsersShow: return "/api/users/\(id)"
+        case .UsersUpdate: return "/api/users/\(id)"
+            
+        case .MicropostsPost: return "/api/microposts"
+        case .MicropostsShow: return "/api/microposts/\(id)"
+        case .MicropostsDelete: return "/api/microposts/\(id)"
+        case .Feed: return "api/feed"
         }
+    }
+    
+    func path() -> String {
+        return path(id: nil)
     }
 }
