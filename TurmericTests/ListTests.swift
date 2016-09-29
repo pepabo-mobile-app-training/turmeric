3c22c6bad150a4b1be40ed825c5df34a844e9a24
ogidow <syuta.ogido@gmail.com>
2016-09-29 16:20:59 +0900

    安全のためオプショナル型にする

--- TurmericTests/ListTests.swift
+++ TurmericTests/ListTests.swift
@@ -1,9 +1,49 @@
-//
-//  ListTests.swift
-//  Turmeric
-//
-//  Created by usr0600438 on 2016/09/29.
-//  Copyright © 2016年 GMO Pepabo. All rights reserved.
-//
+import XCTest
+import OHHTTPStubs
+import Nimble
 
-import Foundation
+@testable import Turmeric
+
+class ListTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        
+        stub(condition: isHost("currry.xyz") && isPath("/api/lists") && isMethodPOST()){_ in
+            return OHHTTPStubsResponse(
+                jsonObject: [
+                    "lists" :[
+                        ["id" : 1, "name" : "friend"],
+                        ["id" : 2, "name" : "curry"]
+                    ]
+                ],
+                statusCode: 200,
+                headers: nil
+            )
+        }
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        OHHTTPStubs.removeAllStubs()
+    }
+    
+    func testMyGetLists() {
+        let userParams = ["user" : ["email" : "syuta_ogido@yahoo.co.jp", "password" : "testtest"]]
+        waitUntil() {done in
+            User.authenticate(parameters: userParams) { response in
+                done()
+            }
+        }
+        
+        waitUntil { done in
+            User.getMyLists { response in
+                response.forEach {
+                    XCTAssertNotNil($0.id)
+                    XCTAssertNotNil($0.name)
+                }
+            }
+        }
+    }
+    
+}
