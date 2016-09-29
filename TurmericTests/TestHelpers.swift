b2a6dacb8ec6cd933802705c67728f641b99c745
ogidow <syuta.ogido@gmail.com>
2016-09-29 19:06:10 +0900

    Turmericターゲットを読み込む

--- TurmericTests/TestHelpers.swift
+++ TurmericTests/TestHelpers.swift
@@ -1,10 +1,11 @@
 import Foundation
 import Nimble
 
+@testable import Turmeric
+
 internal func login(parameters : [String : Any?]){
     waitUntil { done in
         User.authenticate(parameters: parameters) { response in
-            XCTAssertEqual("ThisIsAuthToken", APIClient.token)
             done()
         }
     }
