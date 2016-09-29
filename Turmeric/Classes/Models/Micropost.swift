8b87a9d830c7163e3859c0f7dbf2c06f6dc2b637
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-09-29 16:57:38 +0900

    user_idを追加

--- Turmeric/Classes/Models/Micropost.swift
+++ Turmeric/Classes/Models/Micropost.swift
@@ -3,15 +3,18 @@ import SwiftyJSON
 
 class Micropost {
     let id: Int
+    let userId: Int
     let content: String
 
-    init(id: Int, content: String) {
+    init(id: Int, userId: Int, content: String) {
         self.id = id
+        self.userId = userId
         self.content = content
     }
 
     init(json: JSON) {
         self.id = json["micropost"]["id"].int!
+        self.userId = json["micropost"]["user_id"].int!
         self.content = json["micropost"]["content"].string!
     }
 }
