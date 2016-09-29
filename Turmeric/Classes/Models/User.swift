110cf6da8b665fa15ce5df16ecfaae483dec9529
ry023 <blueno1.aoiroichigo@gmail.com>
2016-09-29 14:23:08 +0900

    field追加

--- Turmeric/Classes/Models/User.swift
+++ Turmeric/Classes/Models/User.swift
@@ -8,7 +8,12 @@ class User {
     let name: String
     let email: String
     
-    init(id: Int, name: String, email: String, token: String? = nil) {
+    var followingCount: Int?
+    var followersCount: Int?
+    var micropostsCount: Int?
+    var iconUrl: NSURL?
+    
+    init(id: Int, name: String, email: String) {
         self.id = id
         self.name = name
         self.email = email
@@ -18,10 +23,21 @@ class User {
         self.id   = json["user"]["id"].int!
         self.name = json["user"]["name"].string!
         self.email = json["user"]["email"].string!
+        
+        self.followingCount  = json["user"]["following_count"].intValue
+        self.followersCount  = json["user"]["followers_count"].intValue
+        self.micropostsCount = json["user"]["microposts_count"].intValue
+        self.iconUrl         = NSURL(string: json["user"]["icon_url"].stringValue)
     }
 
     static func createUser(parameters: Parameters, handler: @escaping ((User) -> Void)) {
         APIClient.request(endpoint: Endpoint.UsersCreate, parameters: parameters) { json in
+            handler(User(json: json))
+        }
+    }
+    
+    static func getMyUser(handler: @escaping ((User) -> Void)) {
+        APIClient.request(endpoint: Endpoint.UsersMe, parameters: [:]) { json in
             handler(User(json: json))
         }
     }
