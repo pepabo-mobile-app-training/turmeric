e2b299ecc23b7ade48046c4e639e1ae5de92efaa
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-09-29 17:12:11 +0900

    pictureを追加

--- Turmeric/Classes/Models/Micropost.swift
+++ Turmeric/Classes/Models/Micropost.swift
@@ -5,16 +5,23 @@ class Micropost {
     let id: Int
     let userId: Int
     let content: String
+    let picture: NSURL?
 
-    init(id: Int, userId: Int, content: String) {
+    init(id: Int, userId: Int, content: String, picture: NSURL?) {
         self.id = id
         self.userId = userId
         self.content = content
+        self.picture = picture
     }
 
     init(json: JSON) {
         self.id = json["micropost"]["id"].int!
         self.userId = json["micropost"]["user_id"].int!
         self.content = json["micropost"]["content"].string!
+        if let picture = json["micropost"]["picture"].string {
+            self.picture = NSURL(string: picture)
+        } else {
+            self.picture = nil
+        }
     }
 }
