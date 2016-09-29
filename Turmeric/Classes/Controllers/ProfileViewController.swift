f752a955084bdcca7018cad60197e61de756397f
ry023 <blueno1.aoiroichigo@gmail.com>
2016-09-29 18:19:01 +0900

    UIコンポーネント関連付け

--- Turmeric/Classes/Controllers/ProfileViewController.swift
+++ Turmeric/Classes/Controllers/ProfileViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var editButton: LinedButton!
+    @IBOutlet weak var micropostsLabel: UILabel!
+    @IBOutlet weak var followersButton: UIButton!
+    @IBOutlet weak var followingButton: UIButton!
+    
+    //var followingButton: UIButton
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        User.getMyUser(){ user in
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
