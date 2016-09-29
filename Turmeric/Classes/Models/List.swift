2d9ce87fd762492a5e652612d7dd9c42879a70f4
ogidow <syuta.ogido@gmail.com>
2016-09-29 12:09:49 +0900

    イニシャライザ定義

---
+++ Turmeric/Classes/Models/List.swift
@@ -0,0 +1,19 @@
+
+import Foundation
+import SwiftyJSON
+
+class List {
+    private let id: Int
+    public var name: String
+
+    init (id: Int, name:String) {
+        self.id = id
+        self.name = name
+    }
+    
+    init (json: JSON) {
+        self.id = json["id"].intValue
+        self.name = json["name"].stringValue
+    }
+    
+}
