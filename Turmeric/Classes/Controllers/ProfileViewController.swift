91127becf8ab96c171a6ee8b32b1fefb017a66a7
ry023 <blueno1.aoiroichigo@gmail.com>
2016-09-29 18:04:02 +0900

    ProfileViewControllerクラス

---
+++ Turmeric/Classes/Controllers/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Turmeric
+//
+//  Created by usr0600437 on 2016/09/29.
+//  Copyright © 2016年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
