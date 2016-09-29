bfd4d471dd150ee2fb05013f64d685fad1313b35
ogidow <syuta.ogido@gmail.com>
2016-09-29 18:04:05 +0900

    値が入っている前提のプロパティはoptionalにしない

--- Turmeric/Classes/Models/List.swift
+++ Turmeric/Classes/Models/List.swift
@@ -2,10 +2,10 @@ import Foundation
 import SwiftyJSON
 
 class List {
-    let id: Int?
-    var name: String?
-
-    init (id: Int?, name: String?) {
+    let id: Int
+    var name: String
+    
+    init (id: Int, name: String) {
         self.id = id
         self.name = name
     }
