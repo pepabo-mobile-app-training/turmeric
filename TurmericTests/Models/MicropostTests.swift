a6664493d81b81a52feed7d9fb8d5b47cf6d43cb
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-09-30 15:13:05 +0900

    先にdone()が書いてある方の呼び出しが終わるともう片方が実行されない可能性がある

--- TurmericTests/Models/MicropostTests.swift
+++ TurmericTests/Models/MicropostTests.swift
@@ -36,7 +36,10 @@ class MicropostTests: XCTestCase {
                 XCTAssertEqual(1, response.userId)
                 XCTAssertEqual("testContent", response.content)
                 XCTAssertEqual("https://example.com/picture.jpg", response.picture?.absoluteString)
+                done()
             }
+        }
+        waitUntil { done in
             Micropost.getMicropost(id: 101) { response in
                 XCTAssertEqual(101, response.id)
                 XCTAssertEqual(2, response.userId)
