32df80daf7a845c5277a26755261200ff0ec55e9
ogidow <syuta.ogido@gmail.com>
2016-09-30 15:35:37 +0900

    リスト一覧を取得し表示

--- Turmeric/Classes/Controllers/ListViewController.swift
+++ Turmeric/Classes/Controllers/ListViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ListViewController: UITableViewController {
     
+    var lists: [List]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        User.getMyLists { lists in
+            self.lists = lists
+            self.tableView.reloadData()
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,12 +26,16 @@ class ListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        if let count = self.lists?.count {
+            return count
+        } else {
+            return 0
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath as IndexPath)
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = self.lists?[indexPath.row].name
         return cell
     }
 }
