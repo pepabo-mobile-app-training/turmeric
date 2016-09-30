969cf1b09c4d0fd3d0fa2cd975db7895fdda4e81
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-09-30 11:44:19 +0900

    Micropostモデルのテストを作成

---
+++ TurmericTests/Models/MicropostTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+import OHHTTPStubs
+import Nimble
+
+@testable import Turmeric
+
+class MicropostTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+}
