666f943932374e43d815aa583917665c334bb613
ogidow <syuta.ogido@gmail.com>
2016-10-03 17:50:56 +0900

    List showのテスト

--- TurmericTests/Models/ListTests.swift
+++ TurmericTests/Models/ListTests.swift
@@ -1,34 +1,27 @@
-//
-//  ListTests.swift
-//  Turmeric
-//
-//  Created by usr0600438 on 2016/10/03.
-//  Copyright © 2016年 GMO Pepabo. All rights reserved.
-//
-
 import XCTest
+import Nimble
+
+@testable import Turmeric
 
 class ListTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        enableHTTPStubs()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        disableHTTPStubs()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testListShow() {
+        waitUntil { done in
+            List.getList(id: 100) { response in
+                XCTAssertEqual(100, response.id)
+                XCTAssertEqual("friend", response.name)
+                done()
+            }
         }
     }
     
