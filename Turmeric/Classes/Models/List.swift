9d44a7706aab8250a3835ed6705a9371a8ad9411
ogidow <syuta.ogido@gmail.com>
2016-10-03 16:39:24 +0900

    apiへのリクエスト

--- Turmeric/Classes/Models/List.swift
+++ Turmeric/Classes/Models/List.swift
@@ -14,4 +14,19 @@ class List {
         self.id = json["id"].int!
         self.name = json["name"].string!
     }
+    
+    static func getList(id: Int, handler: @escaping ((List) -> Void)) {
+        APIClient.request(endpoint: Endpoint.ListShow(id)) { json in
+            handler(List(json: json))
+        }
+    }
+    
+    static func getMembers(id:Int, handler: @escaping ([User]?) -> Void) {
+        APIClient.request(endpoint: Endpoint.ListMembers(id)) { json in
+            let members: [User]? = (json["members"].array?.map {
+                User(json: $0)
+                })
+            handler(members)
+        }
+    }
 }
