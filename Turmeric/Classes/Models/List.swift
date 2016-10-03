0d4bec3fdb71e12101018716bc6f5d1ae3c5cbd2
ogidow <syuta.ogido@gmail.com>
2016-10-03 17:49:17 +0900

    引数が間違ってた

--- Turmeric/Classes/Models/List.swift
+++ Turmeric/Classes/Models/List.swift
@@ -17,14 +17,14 @@ class List {
     
     static func getList(id: Int, handler: @escaping ((List) -> Void)) {
         APIClient.request(endpoint: Endpoint.ListShow(id)) { json in
-            handler(List(json: json))
+            handler(List(json: json["list"]))
         }
     }
     
     static func getMembers(id:Int, handler: @escaping ([User]?) -> Void) {
         APIClient.request(endpoint: Endpoint.ListMembers(id)) { json in
             let members: [User]? = (json["members"].array?.map {
-                User(json: $0)
+                User(json: $0["list"])
                 })
             handler(members)
         }
