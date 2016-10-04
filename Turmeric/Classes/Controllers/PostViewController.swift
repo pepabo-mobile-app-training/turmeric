a48dbe43afb2e5eb46fe39a9b5c19c71a05b2221
ry023 <blueno1.aoiroichigo@gmail.com>
2016-10-04 17:19:57 +0900

    post vcを作成

---
+++ Turmeric/Classes/Controllers/PostViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PostViewController.swift
+//  Turmeric
+//
+//  Created by usr0600437 on 2016/10/04.
+//  Copyright © 2016年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
