0bd4c353e90b72e77b8c4ef20d6177346784b5d2
ogidow <syuta.ogido@gmail.com>
2016-10-04 11:57:48 +0900

    ListDetailViewControllerを追加

--- Turmeric/Classes/Controllers/ListDetailViewController.swift
+++ Turmeric/Classes/Controllers/ListDetailViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ListDetailViewController: UIViewController {
-    
+class ListDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    //この時点で初期化してあげないとエラー出てしまう
     var selectedListId: Int?
+    var list: List? = nil
+    var members: [User]? = nil
     
-    //@IBOutlet weak var listLabel: UILabel!
+    @IBOutlet weak var listNameLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        List.getList(id: self.selectedListId!) { response in
+            self.list = response
+            self.listNameLabel.text = response.name
+        }
+        
+        List.getMembers(id: self.selectedListId!) { response in
+            self.members = [User(id: 1, name: "syuta", iconURL: "https://currry.xyz/assets/rails-c094bc3a4bf50e5bb477109e5cb0d213af27ad75b481c4df249f50974dbeefe6.png")]
+            self.tableView.reloadData()
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let count = self.members?.count {
+            return count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "memberCell", for: indexPath as IndexPath)
+        cell.textLabel?.text = self.members?[indexPath.row].name
+        return cell
     }
 }
