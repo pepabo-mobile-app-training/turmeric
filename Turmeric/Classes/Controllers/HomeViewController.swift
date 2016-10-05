e21a983aa9ef140c457e85ce89e8c46e1c8d8aac
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-10-05 18:26:26 +0900

    タブをデザイン(ひとまずInstagram風)

--- Turmeric/Classes/Controllers/HomeViewController.swift
+++ Turmeric/Classes/Controllers/HomeViewController.swift
@@ -3,6 +3,18 @@ import XLPagerTabStrip
 
 class HomeViewController: ButtonBarPagerTabStripViewController {
     override func viewDidLoad() {
+        // タブのデザイン
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.selectedBarBackgroundColor = UIColor(red: 37/255.0, green: 111/255.0, blue: 206/255.0, alpha: 1.0)
+        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+        settings.style.selectedBarHeight = 2.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemsShouldFillAvailiableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+
         super.viewDidLoad()
         // NavigationBarにButtonBar(タブ)を置く
         buttonBarView.removeFromSuperview()
