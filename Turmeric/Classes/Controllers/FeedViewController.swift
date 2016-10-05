92323833b884982dcb0530200166f7e1b16b410b
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-10-05 17:28:00 +0900

    スワイプタブの表示に最低限必要なメソッドを実装

--- Turmeric/Classes/Controllers/FeedViewController.swift
+++ Turmeric/Classes/Controllers/FeedViewController.swift
@@ -1,6 +1,18 @@
 import UIKit
+import XLPagerTabStrip
 
-class FeedViewController: UITableViewController {
+class FeedViewController: UITableViewController, IndicatorInfoProvider {
+    var itemInfo = IndicatorInfo(title: "Feed")
+
+    init(itemInfo: IndicatorInfo) {
+        self.itemInfo = itemInfo
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -14,6 +26,12 @@ class FeedViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    // IndicatorInfoProviderでが必要なメソッド
+    // タブのタイトルなどの情報を返す
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return itemInfo
     }
 
     // MARK: - Table view data source
--- Turmeric/Classes/Controllers/HomeViewController.swift
+++ Turmeric/Classes/Controllers/HomeViewController.swift
@@ -9,4 +9,15 @@ class HomeViewController: ButtonBarPagerTabStripViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+
+    // それぞれのタブとなるViewControlerを返す
+    // 必ずオーバーライドする必要がある
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        // ViewControlerを作成
+        let child1 = FeedViewController(itemInfo: IndicatorInfo(title: "Home"))
+        let child2 = FeedViewController(itemInfo: IndicatorInfo(title: "Friends"))
+
+        // 配列で返す
+        return [child1, child2]
+    }
 }
