4e846788cc486e3b3513956113f70f06c9ef3be6
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-10-05 16:52:47 +0900

    HomeViewControllerを作成してホーム画面のカスタムクラスに設定

---
+++ Turmeric/Classes/Controllers/HomeViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
