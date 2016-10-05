923a2f138fa32ceec72daa58c069345763a7fb5f
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-10-05 17:38:11 +0900

    Storyboardからはinit?(coder aDecoder: NSCoder)イニシャライザが呼ばれる

--- Turmeric/Classes/Controllers/FeedViewController.swift
+++ Turmeric/Classes/Controllers/FeedViewController.swift
@@ -10,7 +10,7 @@ class FeedViewController: UITableViewController, IndicatorInfoProvider {
     }
 
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
 
     override func viewDidLoad() {
