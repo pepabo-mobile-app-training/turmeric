a32eed824a41f5f3858db97054487d917ed8e15b
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-10-06 12:34:53 +0900

    ホーム画面のレイアウトテスト

--- TurmericUITests/HomeUITests.swift
+++ TurmericUITests/HomeUITests.swift
@@ -11,6 +11,16 @@ class HomeUITests: XCTestCase {
         super.tearDown()
     }
 
-    func testHomeLayout() {
-    }
+    func testHomeTabLayout() {
+        let app = XCUIApplication()
+        // ホーム画面が初期状態で選択されていることをテスト
+        XCTAssert(app.tabBars.buttons["ホーム"].exists)
+        XCTAssert(app.tabBars.buttons["ホーム"].isSelected)
+
+        // 上部のスワイプ切り替えタブ
+        let swipeTab = app.navigationBars["Turmeric.HomeView"].collectionViews
+        XCTAssertEqual(2, swipeTab.cells.count)
+        swipeTab.staticTexts["Friends"].tap()
+        swipeTab.staticTexts["Home"].tap()
+   }
 }
