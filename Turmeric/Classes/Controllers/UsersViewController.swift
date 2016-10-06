f8c5fc341da10c2b4838ebf2157f5084a5e80528
ry023 <blueno1.aoiroichigo@gmail.com>
2016-10-06 12:39:57 +0900

    vcのクラス作成

---
+++ Turmeric/Classes/Controllers/UsersViewController.swift
@@ -0,0 +1,26 @@
+//
+//  UsersViewController.swift
+//  Turmeric
+//
+//  Created by usr0600437 on 2016/10/06.
+//  Copyright © 2016年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+
+    // 遷移元のVCで、遷移前にこのプロパティを設定する
+    var displayUsers: [User] = []
+ 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
