b0843adf52a56b1ee26fe28986ee55457ef4c665
ogidow <syuta.ogido@gmail.com>
2016-10-07 16:16:12 +0900

    viewController追加

---
+++ Turmeric/Classes/Controllers/ListManagementViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ListManagementViewController.swift
+//  Turmeric
+//
+//  Created by usr0600438 on 2016/10/07.
+//  Copyright © 2016年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class ListManagementViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
