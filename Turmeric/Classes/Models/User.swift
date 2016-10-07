28e1b93497c3f1860a38ae50bcabe7058198852c
ogidow <syuta.ogido@gmail.com>
2016-10-07 12:23:33 +0900

    Userモデルはリストにユーザを追加しない

--- Turmeric/Classes/Models/User.swift
+++ Turmeric/Classes/Models/User.swift
@@ -84,10 +84,4 @@ class User {
             handler(users)
         }
     }
-    
-    static func addMember(id: Int, parameters: Parameters, handler: @escaping (User) -> Void) {
-        APIClient.request(endpoint: Endpoint.ListsAddMember(id), parameters: parameters) { json in
-            handler(User(json: json["member"]))
-        }
-    }
 }
