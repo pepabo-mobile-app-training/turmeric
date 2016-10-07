668862c551547786e10a0d64f43b864ba6089617
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-10-07 14:50:07 +0900

    フィードのテスト作成

--- TurmericUITests/FeedUITests.swift
+++ TurmericUITests/FeedUITests.swift
@@ -13,5 +13,22 @@ class FeedUITests: XCTestCase {
 
     func testHomeFeed() {
         let app = XCUIApplication()
+        // ホームフィード(通常のタイムライン)
+        app.tabBars.buttons["ホーム"].tap()
+        let micropostCells = app.tables.cells
+        XCTAssertEqual(10, micropostCells.count)
+        XCTAssert(micropostCells.staticTexts["Writing a short test"].exists)
+    }
+
+    func testListFeed() {
+        let app = XCUIApplication()
+        app.tabBars.buttons["ホーム"].tap()
+        let swipeTab = app.navigationBars["Turmeric.HomeView"].collectionViews
+        // Friendsリストのフィードに移動
+        swipeTab.staticTexts["Friends"].tap()
+        let micropostCells = app.tables.cells
+        // TODO: リストのフィードが取得できるようになったら、リストフィードにしかないテキストがあるかを確認する
+        XCTAssertEqual(10, micropostCells.count)
+        XCTAssert(micropostCells.staticTexts["Writing a short test"].exists)
     }
 }
