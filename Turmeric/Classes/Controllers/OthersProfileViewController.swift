b4a5d53be14c6b7d17226f013af1b9cd0886b616
ry023 <blueno1.aoiroichigo@gmail.com>
2016-10-11 15:53:16 +0900

    フォロー/アンフォローボタンを追加

--- Turmeric/Classes/Controllers/OthersProfileViewController.swift
+++ Turmeric/Classes/Controllers/OthersProfileViewController.swift
@@ -9,15 +9,16 @@
 import UIKit
 
 class OthersProfileViewController: UIViewController {
+    @IBOutlet weak var followButton: LinedButton!
     @IBOutlet weak var followingButton: UIButton!
     @IBOutlet weak var followersButton: UIButton!
     @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var micropostsLabel: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
     
-    
     // 遷移元のVCで、このプロパティに表示したいユーザをsetする
     var user: User? = nil
+    var isFollowedByMe: Bool! = nil
     
     override func viewWillAppear(_ animated: Bool) {
         self.usernameLabel.text = self.user?.name
@@ -34,6 +35,28 @@ class OthersProfileViewController: UIViewController {
             self.profileImage.image = UIImage(data: data)
         } catch {
             //画像がダウンロードできなかった
+        }
+        
+        
+        // 自分がフォローしてるか取得してボタンを変更
+        User.getMyUser(){ me in
+            User.getFollowing(id: me.id){ following in
+                self.followButton.isHidden = false    // レスポンスが来たので隠すのをやめる
+                
+                self.isFollowedByMe = false
+                
+                var title = "フォロー"
+                
+                following?.forEach() { followedByMe in
+                    if(followedByMe.id == self.user?.id){
+                        self.isFollowedByMe = true
+                        title = "アンフォロー"
+                        return
+                    }
+                }
+                
+                self.followButton.setTitle(title, for: UIControlState.normal)
+            }
         }
     }
     
@@ -70,6 +93,22 @@ class OthersProfileViewController: UIViewController {
         default:
             break
         }
+    }
+    
+    @IBAction func followButtonDidTap(_ sender: AnyObject) {
+        // フォロー/アンフォローボタンを押下
+        if(self.isFollowedByMe!){
+            Relationship.destroyRelationship(userID: self.user!.id){
+                self.isFollowedByMe = false
+                self.followButton.setTitle("フォロー", for: UIControlState.normal)
+            }
+        }else{
+            Relationship.createRelationship(userID: self.user!.id){
+                self.isFollowedByMe = true
+                self.followButton.setTitle("アンフォロー", for: UIControlState.normal)
+            }
+        }
+
     }
 
 }
