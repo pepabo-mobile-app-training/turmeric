2f3ff9d8803b1308ed83becda2faea044e31e0cb
ogidow <syuta.ogido@gmail.com>
2016-10-11 11:02:48 +0900

    削除ボタンが押されたリストを保持する

--- Turmeric/Classes/Controllers/ListManagementViewController.swift
+++ Turmeric/Classes/Controllers/ListManagementViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ListManagementViewController: UITableViewController{
 
+    var myLists: [List] = []
+    var deleteLists: [List] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.register(UINib(nibName: "ListDeleteCell", bundle: nil), forCellReuseIdentifier: "listDeleteCell")
-        self.tableView.reloadData()
+        List.getMyLists { response in
+            self.myLists = response
+            self.tableView.reloadData()
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -26,12 +32,41 @@ class ListManagementViewController: UITableViewController{
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.myLists.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "listDeleteCell", for: indexPath) as! ListDeleteCell
-        cell.name.text = "Friends"
+        
+        cell.name.text = self.myLists[indexPath.row].name
+        
+        let deleteButton = cell.deleteButton!
+        //削除ボタンタップ時のコールバック設定
+        deleteButton.addTarget(self, action: #selector(ListManagementViewController.deleteButtonTap), for: .touchDown)
+        
         return cell
+    }
+    
+    override func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
+        let cell = self.tableView.cellForRow(at: indexPath)!
+        // セル内で削除ボタン以外がタッチされても背景を変えない
+        cell.selectionStyle = .none
+    }
+    
+    func deleteButtonTap(sender: UIButton, event: UIEvent) {
+        if let touch = event.allTouches?.first {
+            // タッチされたボタンの位置からindexPathを検出
+            let point = touch.location(in: self.tableView)
+            let indexPath = self.tableView.indexPathForRow(at: point)
+            let cell = self.tableView.cellForRow(at: indexPath!) as! ListDeleteCell
+            
+            // 削除ボタンの該当ユーザをdeleteMemberとして保存する
+            self.deleteLists.append(self.myLists[(indexPath?.row)!])
+            
+            // 該当セルの背景をグレイにし、削除ボタンを非表示にする
+            cell.selectionStyle = .none
+            cell.backgroundColor = UIColor.lightGray
+            cell.deleteButton.isHidden = true
+        }
     }
 }
