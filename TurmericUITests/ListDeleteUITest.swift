e48c63ba3b1e9cc2260eb3a35366c9af0a8d31c4
ogidow <syuta.ogido@gmail.com>
2016-10-11 12:28:24 +0900

    UIテスト

---
+++ TurmericUITests/ListDeleteUITest.swift
@@ -0,0 +1,40 @@
+//
+//  ListDeleteUITest.swift
+//  Turmeric
+//
+//  Created by usr0600438 on 2016/10/11.
+//  Copyright © 2016年 GMO Pepabo. All rights reserved.
+//
+
+import XCTest
+
+class ListDeleteUITest: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testListDeleteLayout() {
+        let app = XCUIApplication()
+        app.tabBars.buttons["リスト"].tap()
+        app.buttons["追加・削除"].tap()
+        
+        let listCell = app.tables.cells.element(boundBy: 0)
+        let deleteButton = listCell.buttons["deleteButton"]
+        let complateButton = app.buttons["完了"]
+        
+        XCTAssert(deleteButton.exists)
+        XCTAssert(complateButton.exists)
+        
+        deleteButton.tap()
+        complateButton.tap()
+        
+    }
+    
+}
