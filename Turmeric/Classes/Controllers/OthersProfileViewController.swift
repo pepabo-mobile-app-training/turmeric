ded267b05c0ed92e64f19f8bd622fa317c13ace2
ry023 <blueno1.aoiroichigo@gmail.com>
2016-10-11 12:19:45 +0900

    vc内動作

--- Turmeric/Classes/Controllers/OthersProfileViewController.swift
+++ Turmeric/Classes/Controllers/OthersProfileViewController.swift
@@ -17,7 +17,32 @@ class OthersProfileViewController: UIViewController {
     
     
     // 遷移元のVCで、このプロパティに表示したいユーザをsetする
-    var user: User?
+    var user: User? {
+        set {
+            self.userVal = newValue
+            
+            self.usernameLabel.text = newValue.name
+            
+            if let micropostCount = newValue.micropostsCount, let followersCount = newValue.followersCount, let followingCount = newValue.followingCount {
+                self.micropostsLabel.text = micropostCount.description
+                
+                self.followersButton.setTitle(followersCount.description, for: UIControlState.normal)
+                self.followingButton.setTitle(followingCount.description, for: UIControlState.normal)
+            }
+            
+            do {
+                let data = try Data(contentsOf: newValue.iconURL )
+                self.profileImage.image = UIImage(data: data)
+            } catch {
+                //画像がダウンロードできなかった
+            }
+        }
+        
+        get {
+            return self.userVal
+        }
+    }
+    private var userVal: User? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,4 +51,32 @@ class OthersProfileViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let segueID = segue.identifier else {
+            return
+        }
+        
+        switch(segueID){
+        case "following":
+            let vc = segue.destination as! UsersViewController
+            
+            // 次のvcにフォローユーザたちを表示するように依頼
+            User.getFollowing(id: user.id){ following in
+                vc.displayUsers = following!
+            }
+            break
+        case "followers":
+            let vc = segue.destination as! UsersViewController
+            
+            // 次のvcにフォロワーたちを表示するように依頼
+            User.getFollowers(id: user.id){ followers in
+                vc.displayUsers = followers!
+            }
+            break
+        default:
+            break
+        }
+    }
+
 }
