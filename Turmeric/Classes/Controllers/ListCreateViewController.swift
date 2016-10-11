fcec0fba1510a375086c07021e6ac398679b9efc
ogidow <syuta.ogido@gmail.com>
2016-10-11 13:02:01 +0900

    viewController作成

---
+++ Turmeric/Classes/Controllers/ListCreateViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ListCreateViewController.swift
+//  Turmeric
+//
+//  Created by usr0600438 on 2016/10/11.
+//  Copyright © 2016年 GMO Pepabo. All rights reserved.
+//
+
+import UIKit
+
+class ListCreateViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
