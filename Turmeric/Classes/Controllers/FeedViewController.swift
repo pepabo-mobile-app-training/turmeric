e1a2f5ecbc21863c2738273ddbf6ea3309647b0c
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-10-11 11:29:53 +0900

    仮の画像を表示する

--- Turmeric/Classes/Controllers/FeedViewController.swift
+++ Turmeric/Classes/Controllers/FeedViewController.swift
@@ -34,7 +34,8 @@ class FeedViewController: UITableViewController, IndicatorInfoProvider {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "micropostCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "micropostCell", for: indexPath) as! MicropostCell
+        cell.profileImage.af_setImage(withURL: URL(string: "https://secure.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af?s=80")!)
 
         return cell
     }
