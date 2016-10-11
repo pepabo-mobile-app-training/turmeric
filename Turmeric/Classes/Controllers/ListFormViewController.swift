f7185010f39ebb34d08b79e79ef6370e554e57cd
ogidow <syuta.ogido@gmail.com>
2016-10-11 15:20:03 +0900

    フォーム作成

--- Turmeric/Classes/Controllers/ListFormViewController.swift
+++ Turmeric/Classes/Controllers/ListFormViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ListCreateViewController.swift
+//  FormViewController.swift
 //  Turmeric
 //
 //  Created by usr0600438 on 2016/10/11.
@@ -9,11 +9,13 @@
 import UIKit
 import Eureka
 
-class ListCreateViewController: UIViewController {
+class ListFormViewController:  FormViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        form +++ TextRow() {
+            $0.title = "リスト名"
+        }
     }
     
     override func didReceiveMemoryWarning() {
