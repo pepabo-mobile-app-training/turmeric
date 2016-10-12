e86d2c0c57cf9c19643c4ef16a946dc9843f05a8
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-10-12 18:45:47 +0900

    ISO8601形式の日付を扱いたいのでDateクラスを拡張

---
+++ Turmeric/Classes/Helpers/Date.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+extension Date {
+    struct Formatter {
+        static let iso8601: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.calendar = Calendar(identifier: .iso8601)
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+            formatter.timeZone = TimeZone(secondsFromGMT: 0)
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+            return formatter
+        }()
+    }
+    var iso8601: String {
+        return Formatter.iso8601.string(from: self)
+    }
+}
+
+extension String {
+    var dateFromISO8601: Date? {
+        return Date.Formatter.iso8601.date(from: self)
+    }
+}
