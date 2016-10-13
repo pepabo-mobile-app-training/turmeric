b46ebab724b7cdc533e7bf9d95d90377c41a1027
ogidow <syuta.ogido@gmail.com>
2016-10-13 19:17:26 +0900

    サーバーからのメッセージを表示

--- Turmeric/Classes/Controllers/LoginViewController.swift
+++ Turmeric/Classes/Controllers/LoginViewController.swift
@@ -8,28 +8,40 @@
 
 import UIKit
 import Eureka
+import SwiftyJSON
 
 class LoginViewController: FormViewController{
     let messageDisplay: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        LabelRow.defaultCellUpdate = { cell, row in
+            cell.contentView.backgroundColor = .red
+            cell.textLabel?.textColor = .white
+            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+            cell.textLabel?.textAlignment = .right
+            
+        }
+        
         if (APIClient.token != nil) {
             //すでにログインしていればhomeに遷移
             self.performSegue(withIdentifier: "mainView", sender: self)
         }
         
-        form +++ EmailRow("email") { row in
+        form +++ Section()
+        <<< EmailRow("email") { row in
             row.title = "メールアドレス"
         }
-            <<< PasswordRow("password") { row in
+        <<< PasswordRow("password") { row in
             row.title = "パスワード"
         }
-            +++ ButtonRow("loginButton") {row in
+        +++ Section()
+        <<< ButtonRow("loginButton") {row in
             row.title = "ログイン"
         }.onCellSelection({cell, row in
             let emailRow: EmailRow? = self.form.rowBy(tag: "email")
             let passwordRow: PasswordRow? = self.form.rowBy(tag: "password")
+            row.section?.form?.validate()
             
             if let email = emailRow?.value, let password = passwordRow?.value {
                 let parameters = ["user": ["email": email, "password": password]]
@@ -37,7 +49,17 @@ class LoginViewController: FormViewController{
                     switch response {
                     case .Success:
                         self.performSegue(withIdentifier: "mainView", sender: self)
-                    case .ValidationError(let json): break
+                    case .ValidationError(let err):
+                        print(err)
+                        
+                        print(err["message"].string)
+                        let section = self.form.first
+                        section! <<< LabelRow() {row in
+                            row.title = err["message"].string
+                            
+                        }
+                        
+                        
                     default: break
                     }
                 }
