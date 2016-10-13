8b8d5daeab80e8e115ecfe7b16029daaca4c101c
ry023 <blueno1.aoiroichigo@gmail.com>
2016-10-13 15:08:41 +0900

    ラベル紐つけ

--- Turmeric/Classes/Views/MicropostCell.swift
+++ Turmeric/Classes/Views/MicropostCell.swift
@@ -4,7 +4,8 @@ class MicropostCell: UITableViewCell {
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var content: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
-
+    @IBOutlet weak var date: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
