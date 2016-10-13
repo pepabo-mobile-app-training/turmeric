8fa21a04502cfd45e8a39905264357432c27ef8d
ogidow <syuta.ogido@gmail.com>
2016-10-13 14:25:03 +0900

    ログイン処理

--- Turmeric/Classes/Controllers/LoginViewController.swift
+++ Turmeric/Classes/Controllers/LoginViewController.swift
@@ -10,17 +10,36 @@ import UIKit
 import Eureka
 
 class LoginViewController: FormViewController{
+    let messageDisplay: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        form +++ TextRow("listName") { row in
+        form +++ EmailRow("email") { row in
             row.title = "メールアドレス"
         }
             <<< PasswordRow("password") { row in
             row.title = "パスワード"
         }
-            +++ ButtonRow() {row in
+            +++ ButtonRow("loginButton") {row in
             row.title = "ログイン"
-        }
+        }.onCellSelection({cell, row in
+            let emailRow: EmailRow? = self.form.rowBy(tag: "email")
+            let passwordRow: PasswordRow? = self.form.rowBy(tag: "password")
+            print ("tapped button")
+            if let email = emailRow?.value, let password = passwordRow?.value {
+                let parameters = ["user": ["email": email, "password": password]]
+                User.authenticate(parameters: parameters){ response in
+                    switch response {
+                    case .Success:
+                        print("logged in")
+                        self.performSegue(withIdentifier: "mainView", sender: self)
+                    case .ValidationError(let json):
+                        print(json)
+                    default: break
+                    }
+                }
+            }
+        })
     }
     
     override func didReceiveMemoryWarning() {
